d5f839038b27a7a6ff3ddf410981d0a0dde7d5b9
Albert Li <z5264717@ad.unsw.edu.au>
2021-07-07 12:27:31 +1000

    Now displays name of the  person who made the comment

--- website/review.py
+++ website/review.py
@@ -3,7 +3,7 @@ from flask import Blueprint, request, flash, jsonify
 from flask_login import login_required, current_user
 import json
 
-from .models import Comments, Recipes
+from .models import Comments, Profiles
 from . import db
 
 review = Blueprint('review', __name__)
@@ -21,7 +21,8 @@ def create_comment():
         if len(comment) < 1:
             flash('Comment is too short!', category='error')
         else:
-            print(current_user.id)
+            profile = Profiles.query.filter_by(owns=current_user.id).first()
+            print(profile.display_name)
             new_comment = Comments(comment=comment, has=recipe_id, owns=current_user.id)
             db.session.add(new_comment)
             db.session.commit()
@@ -30,7 +31,16 @@ def create_comment():
 
 # Retrieves comments given recipe_id
 def retrieve_comments(recipe_id):
-    return Comments.query.filter_by(has = recipe_id).all()
+    # Create a list that stores both the display_name of comment creator and comments themselves.
+    name_and_list_comments = []
+    list_comments = Comments.query.filter_by(has = recipe_id).all()
+    for comments in list_comments:
+        # Instead of adding display name to comments table, we find it here given comments.own
+        # Creates a lot of overhead right now, needs to be fixed later
+        profile = Profiles.query.filter_by(owns=comments.owns).first()
+        name_and_comment = (profile.display_name, comments)
+        name_and_list_comments.append(name_and_comment)
+    return name_and_list_comments
 
 @review.route('/delete-comment', methods=['POST'])
 def delete_comment():
@@ -62,3 +72,13 @@ def modify_comment():
             flash('Modified comment successfully!', category='success')
 
         return jsonify({})
+
+@review.route('/add-like', methods=['POST'])
+def add_like():
+    print("Route for add_like works")
+    return jsonify({})
+
+@review.route('/add-dislike', methods=['POST'])
+def add_dislike():
+    print("Route for add_dislike works")
+    return jsonify({})
