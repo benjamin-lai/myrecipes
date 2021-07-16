058a2ca5e959533b4e9278efe89696a57e9b033b
Albert Li <z5264717@ad.unsw.edu.au>
2021-07-16 13:41:53 +1000

    Added image attachment to support email

--- website/support.py
+++ website/support.py
@@ -2,20 +2,30 @@ from flask import Blueprint, render_template, flash, request, current_app as app
 from flask_login import login_required, current_user
 from flask_cors import CORS
 from flask_mail import Mail, Message
+import boto3
+from werkzeug.utils import secure_filename
+from os import getcwd, chdir
 
-from . import db
 from .models import Users, Profiles, Newsfeeds
+
+s3 = boto3.client('s3',
+                    aws_access_key_id='AKIAQNR7WVADC7MX2ZEW',
+                    aws_secret_access_key= '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'
+                     )
+BUCKET_NAME='comp3900-w18b-sheeesh'
 
 
 support = Blueprint('support', __name__)
 CORS(support)
 
 @support.route('/support', methods=['GET', 'POST'])
+@login_required
 def supports():
     if request.method == 'POST':
         # Getting list of integers, that correspond to content they want in their newsletters
         topic = request.form.get('topic_select')
         body = request.form['email-body']
+        uploaded_files = request.files.getlist('files')
         
         if topic is None:
             flash('Please select a topic', category='error')
@@ -24,16 +34,36 @@ def supports():
             flash('Please enter some description', category='error')
 
         else:
+            # Given the uploaded_files save it into boto3
+            for f in uploaded_files:
+                if f:
+                    filename = secure_filename(f.filename)
+                    f.save(filename)
+                    s3.upload_file(
+                        Bucket = 'comp3900-w18b-sheeesh',
+                        Filename=filename,
+                        Key = filename
+                    )
+
             # Send information over to our email.
             flash('Successfully sent email!', category='success')
-            send_email(current_user.email, topic, body)
+            send_email(current_user.email, topic, body, uploaded_files)
 
     return render_template("support.html", user=current_user)
 
-def send_email(email, topic, body):
+def send_email(email, topic, body, files):
     mail = Mail(app)
     msg = Message(topic,
         sender=email,
         recipients=['w18b.sheeesh@gmail.com'])
     msg.body = body
+
+    # Go through the attached files again and attach it to our email.
+    for f in files:
+        if f:
+            # Forced to use local images rather than boto3. Since msg.attach takes a 
+            #   file rather than a link to our cloud.
+            with open(f.filename, 'rb') as images:
+                msg.attach(f.filename, f.content_type, images.read())
+            
     mail.send(msg)
