3fd5b5b975c18b14b05f80caef58593c4310dcf0
Albert Li <z5264717@ad.unsw.edu.au>
2021-07-16 15:06:38 +1000

    Added a restricted access webpage that redirects users back to login page.

--- website/support.py
+++ website/support.py
@@ -4,9 +4,6 @@ from flask_cors import CORS
 from flask_mail import Mail, Message
 import boto3
 from werkzeug.utils import secure_filename
-from os import getcwd, chdir
-
-from .models import Users, Profiles, Newsfeeds
 
 s3 = boto3.client('s3',
                     aws_access_key_id='AKIAQNR7WVADC7MX2ZEW',
@@ -19,9 +16,8 @@ support = Blueprint('support', __name__)
 CORS(support)
 
 @support.route('/support', methods=['GET', 'POST'])
-@login_required
 def supports():
-    if request.method == 'POST':
+    if current_user.is_authenticated and request.method == 'POST':
         # Getting list of integers, that correspond to content they want in their newsletters
         topic = request.form.get('topic_select')
         body = request.form['email-body']
@@ -49,7 +45,8 @@ def supports():
             flash('Successfully sent email!', category='success')
             send_email(current_user.email, topic, body, uploaded_files)
 
-    return render_template("support.html", user=current_user)
+        return render_template("support.html", user=current_user)
+    return render_template("restricted_access.html")
 
 def send_email(email, topic, body, files):
     mail = Mail(app)
