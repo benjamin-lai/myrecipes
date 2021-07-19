a09788913c98fb1bdc14b847909a22bce2de498f
Albert Li <z5264717@ad.unsw.edu.au>
2021-07-19 11:44:23 +1000

    Partially implemented google signin api

--- website/static/index.js
+++ website/static/index.js
@@ -1,3 +1,18 @@
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log("ID: " + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log("Name: " + profile.getName());
+  console.log("Image URL: " + profile.getImageUrl());
+  console.log("Email: " + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
+
+function signOut() {
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function () {
+    console.log("User signed out.");
+  });
+}
+
 function createComment2(recipe_id) {
   var new_comment = prompt("Create a new comment on this recipe.");
   if (new_comment != null) {
